9708a272454a6bb16db1bf3ef9e258d8cb1a0771
Alvis L. Owens <61351458+Stringbender78@users.noreply.github.com>
2025-02-11 17:54:49 -0500

    Update aloGameEngine.h

--- aloGameEngine.h
+++ aloGameEngine.h
@@ -30,7 +30,7 @@ public:
 		// Called once per frame, draws random colored pixels
 		for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+				Draw(x, y, alo::Pixel(rand() % 256, rand() % 256, rand() % 256));
 		return true;
 	}
 };
